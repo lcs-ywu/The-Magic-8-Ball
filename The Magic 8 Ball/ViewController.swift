036403aa86a6fe06399b070d9223de06dadb30e6
James Wu <ywu@lcs.on.ca>
2019-11-13 17:09:49 -0500

    Initial Commit

---
+++ The Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Magic 8 Ball
+//
+//  Created by James Wu on 2019-11-13.
+//  Copyright © 2019 James Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
